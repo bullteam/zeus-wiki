b151d235b280c7b81bebe37389ab0010db932d39
coso <wutongci@hotmail.com>
2019-04-26 22:29:50 +0800

    更换logo

--- Zeus/service-worker.js
+++ Zeus/service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "b8b0f03fe949d21c0fcc0551e60b2a95"
+    "revision": "c9446ee127f9fd1105f201de1620bdea"
   },
   {
     "url": "assets/css/0.styles.816bdec5.css",
@@ -44,16 +44,72 @@ self.__precacheManifest = [
     "revision": "0f86a7af111406c860a037a5da83a5fe"
   },
   {
-    "url": "assets/js/app.a95618cb.js",
-    "revision": "983297481f33aadeecc480b948dd4235"
+    "url": "assets/js/app.eb0ddee2.js",
+    "revision": "e9248be849a601595c56382fb7ca9525"
   },
   {
     "url": "bull.png",
     "revision": "37b7f689b04a7a593aa754c46245a3bd"
   },
   {
+    "url": "icons/android-chrome-192x192.png",
+    "revision": "5cc7e9c000523749a298930b881dbac2"
+  },
+  {
+    "url": "icons/android-chrome-512x512.png",
+    "revision": "85680b8c664e0720bb5f7b807e8c59db"
+  },
+  {
+    "url": "icons/apple-touch-icon-120x120.png",
+    "revision": "672c2546ced2ba4b6273b923c67523e4"
+  },
+  {
+    "url": "icons/apple-touch-icon-152x152.png",
+    "revision": "8a674f0ec80365815f6721b6906515c4"
+  },
+  {
+    "url": "icons/apple-touch-icon-180x180.png",
+    "revision": "24b790bdb52985d90ff85542f4e5a9c3"
+  },
+  {
+    "url": "icons/apple-touch-icon-60x60.png",
+    "revision": "d1ea90cd304c069cd9cd5321153e6539"
+  },
+  {
+    "url": "icons/apple-touch-icon-76x76.png",
+    "revision": "66f5d072ab6e9ac15a6bdbcfc2965ba4"
+  },
+  {
+    "url": "icons/apple-touch-icon.png",
+    "revision": "24b790bdb52985d90ff85542f4e5a9c3"
+  },
+  {
+    "url": "icons/favicon-16x16.png",
+    "revision": "55fff24d6476f7a57d51016056a61011"
+  },
+  {
+    "url": "icons/favicon-32x32.png",
+    "revision": "06bb362822028a5907303792da098da4"
+  },
+  {
+    "url": "icons/msapplication-icon-144x144.png",
+    "revision": "bafedf066bd2414aec570b086e08b5f0"
+  },
+  {
+    "url": "icons/mstile-150x150.png",
+    "revision": "907456ec302a7d44b579cdc3f3b39183"
+  },
+  {
+    "url": "icons/safari-pinned-tab.svg",
+    "revision": "f22d501a35a87d9f21701cb031f6ea17"
+  },
+  {
     "url": "index.html",
-    "revision": "3cf2bb85de9ccd7133b29db7f5fb0a53"
+    "revision": "89055b1e5d770c7d684d397ba3a87762"
+  },
+  {
+    "url": "logo.png",
+    "revision": "85680b8c664e0720bb5f7b807e8c59db"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,7 @@ module.exports = {
         description: 'Unified Authority Control Background'
       }
     },
-   /* head: [
+    head: [
       ['link', { rel: 'icon', href: `/logo.png` }],
       ['link', { rel: 'manifest', href: '/manifest.json' }],
       ['meta', { name: 'theme-color', content: '#3eaf7c' }],
@@ -22,7 +22,7 @@ module.exports = {
       ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
       ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
       ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
-    ],*/
+    ],
     serviceWorker: true,
     theme: 'vue',
     themeConfig: {
